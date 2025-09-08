950bd1ccc1d256b44fda29f96a35eaf5ca2b7f25
Villarrealucas <144961720+Villarrealucas@users.noreply.github.com>
2025-09-08 12:01:43 -0500

    Add files via upload

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/ITD/subsystems/wrist.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.ITD.subsystems;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@Config
+public class wrist {
+    public final double intakePos = 0;
+    private static Servo wristServo;
+    private static Gamepad Driver1;
+    public  final double specimenIntake = 0.14;
+
+    public wrist(OpMode opMode) {
+        Driver1 = opMode.gamepad1;
+        wristServo = (Servo) opMode.hardwareMap.get("Wrist");
+        wristServo.setDirection(Servo.Direction.REVERSE);
+        wristServo(specimenIntake, specimenIntake);
+    }
+
+    public  void teleOp() {
+        if (Driver1.left_bumper){
+            wristServo.setPosition(intakePos);}
+
+        if (Driver1.right_bumper) {
+            wristServo(specimenIntake, specimenIntake);
+        }
+    }
+    public  void wristServo(double setPositionRight, double setPositionLeft) {
+        wristServo.setPosition(setPositionLeft);
+    }}
